1a873494640432ee13b47043cc04b19dea92efef
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-10-26 14:49:49 +1300

    Add number loops

--- whileLoopLearn.py
+++ whileLoopLearn.py
@@ -37,26 +37,43 @@
 
 # ---------------------------------------------------------------------
 
-number_of_tries_names = 3
-name = ""
-while len(name) == 0:
-  name = input("Enter your name: ")
-  number_of_tries_names -= 1
+# number_of_tries_names = 3
+# name = ""
+# while len(name) == 0:
+#   name = input("Enter your name: ")
+#   number_of_tries_names -= 1
   
-  if number_of_tries_names == 0:
-    print(" You are out of goes. Sorry")
-    if len(name) > 0:
-      print (f"Hello {name}") 
+#   if number_of_tries_names == 0:
+#     print(" You are out of goes. Sorry")
+#     if len(name) > 0:
+#       print (f"Hello {name}") 
 
-    break
+#     break
   
-    print("Press Entre to exit the loop")
+# print("Press Entre to exit the loop")
 
 
 # ---------------------------------------------------------------------
 
+# Write a program with a while loop that prints 1 through to n in square brackets. 
+# For example, if the user enters 6 then the program should display4
+# [1] [2] [3] [4] [5] [6]
+
+
+number = 0
+new_number = int(input("Insert a number to start the loop..\n"))
+
+while number < new_number:
+  print("[",number + 1,"] ")
+  number += 1
+ 
+
 
   
+
+ 
+
+
  
   
 
