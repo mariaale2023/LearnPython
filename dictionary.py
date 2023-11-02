31b0bc794e6cd65e10c8292eb11300d70ec088e3
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-03 11:36:26 +1300

    add file dictonary file

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+dictionary_1 = {"V344LDA":2000,
+                "J245DWE":6350,
+                "K265QWS":500}
+
+print("\n\n The value of the car registracion of V344LDA is", dictionary_1["V344LDA"])
