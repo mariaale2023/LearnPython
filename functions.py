7753d16ca8cc3f0b00e1dccd75d2d0acf79364b3
Mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-11-09 11:43:57 +1300

    add string learning

--- functions.py
+++ functions.py
@@ -87,20 +87,20 @@ print("---------Other option------------")
 # score = show_new_score(parameter)
 
 
-score = 4
+# score = 4
  
-def get_new_score(param_score):
-    param_score += 1
-    print("You got another point...\n"
-          "Your score is now {0}.\n"
-          .format(param_score))
-    return param_score
-# some run code
-x = int(input("insert a number"))
-# invoking the function and using it to set the new score
-score = x + get_new_score(score)
-score = x + get_new_score(score)
-score = x + get_new_score(score)
+# def get_new_score(param_score):
+#     param_score += 1
+#     print("You got another point...\n"
+#           "Your score is now {0}.\n"
+#           .format(param_score))
+#     return param_score
+# # some run code
+# x = int(input("insert a number"))
+# # invoking the function and using it to set the new score
+# score = x + get_new_score(score)
+# score = x + get_new_score(score)
+# score = x + get_new_score(score)
 
 print("---------------------")
 name = "Maria"
@@ -112,3 +112,79 @@ def show_name():
 print(show_name())
 show_name()
 print(name)
+
+print("---------------------")
+
+def show_age(age):
+  print('Myage 10 year ago was {0} \n'
+  .format(age - 10))
+  
+
+show_age(30)
+
+print("---------------------")
+def show_young_age(my_age, my_name):
+    print("My age 10 years ago was {0}.\n"
+          "My name is {1}."
+          .format(my_age - 10, my_name))
+ 
+# invoking function
+show_young_age(22, "Tuheitia")
+
+print("---------------------")
+
+# Remember that functions have their own scope. Variables declared in a function only have scope in the function block.
+def example_function():
+    # Variable 'local_var' is declared within the function's scope
+    local_var = 42
+    print("Inside the function: local_var =", local_var)
+
+# Call the function
+example_function()
+
+# Try to access 'local_var' outside the function (this will result in an error)
+# Uncommenting the line below will raise a NameError
+# print("Outside the function: local_var =", local_var)
+
+print("---------------------")
+
+#String
+
+string_1 = "Wuaaauuu!!!"
+print("The third letter of string_1 is {0}\n".format(string_1[2]))
+print("The length of string_1 is {0}\n".format(len(string_1)))
+print("The letter 'u' appears {0} times\n".format(string_1.count("u")))
+
+string_2 = "here is a translation: Haera mai ki konei means come here!"
+
+konei_endindex = string_2.find("konei")
+print(konei_endindex)
+# konei_endindex = string_1.find("konei") + len("konei")
+# print("The end index position of konei is {0}\n"
+#       .format(konei_endindex))
+
+
+here_position = string_2.find("here", konei_endindex, len(string_2))
+print("Looking for the string_2 here, anytime after konei...\n\n"
+      "The string_2 here appears at index position..{0}"
+      .format(here_position))
+
+print("---------------------")
+string_3 = "Ka mate kāinga tahi ka ora kāinga rua."
+ 
+print("Does this string start with Ka?....{0}\n".format(string_3.startswith("Ka")))
+ 
+print("Does this string start with Tr?....{0}\n".format(string_3.startswith("Tr")))
+ 
+print("Does this string end with rua?....{0}\n".format(string_3.endswith("rua.")))
+
+print(string_3.isalnum())    #check if all char are alphanumeric 
+print(string_3.isalpha())         #check if all char in the string are alphabetic
+print(string_3.isdigit())         #test if string contains digits
+print(string_3.istitle())         #test if string contains title words
+print(string_3.isupper())         #test if string contains upper case
+print(string_3.islower())         #test if string contains lower case
+print(string_3.isspace())         #test if string contains spaces
+ 
+
+ 
